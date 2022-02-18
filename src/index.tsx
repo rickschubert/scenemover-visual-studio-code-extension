154609bfa03b30eb3bcba99893e9a31d5e31c203
rickschubert <rick.schubert@youview.com>
2022-02-18 16:29:17 +0000

    Adding react-redux from an online boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider as ReduxProvider } from 'react-redux';
+import configureAppStore, { getPreloadedState } from './store/configureStore';
+import AppContextProvider from './contexts/AppContextProvider';
+
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+
+
+(async () => {
+    const preloadedState = getPreloadedState();
+    const appComponent = (
+        <React.StrictMode>
+            <ReduxProvider store={configureAppStore(preloadedState)}>
+                <AppContextProvider>
+                    <App />
+                </AppContextProvider>
+            </ReduxProvider>
+        </React.StrictMode>
+
+    )
+    ReactDOM.render(appComponent, document.getElementById("root") as HTMLElement);
+})();
