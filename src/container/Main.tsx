3861ec736bbfd20e68354ee92a6d59178900021b
rickschubert <rick.schubert@youview.com>
2022-02-19 13:33:33 +0000

    I can get the scenes at the start and set on state

--- src/container/Main.tsx
+++ src/container/Main.tsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { useDispatch } from 'react-redux';
 import Topbar from "../components/Topbar";
 import Scene from "../components/Scene";
 import { getScenes } from "../api/getScenes";
+import {
+  setScenes,
+} from '../store/reducers/UI';
 
 export const Main = () => {
 
+  const dispatch = useDispatch();
+
   const getScenesAsync = async () => {
     getScenes().then((loadedScenes) => {
-      console.log({loadedScenes})
+      dispatch(setScenes());
     })
   }
 
   React.useEffect(() => {
     getScenesAsync()
-  })
+  }, [])
 
   return (
     <>
