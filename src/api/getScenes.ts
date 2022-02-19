f0190f580f1ee69a918396a36c48d1ae54c81dc5
rickschubert <rick.schubert@youview.com>
2022-02-19 13:29:49 +0000

    I can get the scenes at the first render of the component and print out - but how can I now set it on the state?

--- src/api/getScenes.ts
+++ src/api/getScenes.ts
@@ -5,5 +5,6 @@ export interface Scene {
 
 export const getScenes = async (): Promise<Scene[]> => {
     const response = await fetch('http://localhost:8080/scenes')
-    return JSON.stringify(response.text()) as unknown as Scene[]
+    const responseBody = await response.text()
+    return JSON.stringify(responseBody) as unknown as Scene[]
 }
--- src/container/Main.tsx
+++ src/container/Main.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Topbar from "../components/Topbar";
 import Scene from "../components/Scene";
+import { getScenes } from "../api/getScenes";
 
 export const Main = () => {
+
+  const getScenesAsync = async () => {
+    getScenes().then((loadedScenes) => {
+      console.log({loadedScenes})
+    })
+  }
+
+  React.useEffect(() => {
+    getScenesAsync()
+  })
+
   return (
     <>
         <Topbar />
